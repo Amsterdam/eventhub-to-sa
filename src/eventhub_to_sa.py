3f6f619e0d5722e70a0fd59b3d3001312fb7678c
Thomas Kok <thomas.kok@amsterdam.nl>
2024-03-19 10:43:37 +0100

    removed hardcoded params

---
+++ src/eventhub_to_sa.py
@@ -0,0 +1,69 @@
+import nest_asyncio
+
+from datetime import datetime
+from azure.eventhub.aio import EventHubConsumerClient
+from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
+from azure.identity.aio import DefaultAzureCredential
+
+nest_asyncio.apply()
+
+CACHE = {}
+# Adjust to 15 minutes? before running on prod
+MINUTES_BEFORE_FLUSHING_TO_SA = 0.2
+START_SCRIPT_DATE_TIME = datetime.now()
+
+
+async def on_event_batch(partition_context, event_batch):
+    on_event_batch_date_time = datetime.now()
+    print(f"Received event from partition: {partition_context.partition_id}. {len(event_batch)}")
+    if partition_context.partition_id not in CACHE:
+        print("not in cache")
+        CACHE[partition_context.partition_id] = {}
+        CACHE[partition_context.partition_id]['last_flush_datetime'] = START_SCRIPT_DATE_TIME
+        CACHE[partition_context.partition_id]['cached_events'] = event_batch
+    else:
+        print("in cache")
+        CACHE[partition_context.partition_id]['cached_events'].extend(event_batch)
+        print(len(CACHE[partition_context.partition_id]['cached_events']))
+
+    if (on_event_batch_date_time - CACHE[partition_context.partition_id][
+        'last_flush_datetime']).seconds > MINUTES_BEFORE_FLUSHING_TO_SA * 60:
+        print("!!!!flush to storage account and updateoffset!!!!")
+        # TODO store data
+
+        if len(CACHE[partition_context.partition_id]['cached_events']) > 0:
+            await partition_context.update_checkpoint(CACHE[partition_context.partition_id]['cached_events'][-1])
+        CACHE[partition_context.partition_id]['cached_events'] = []
+        CACHE[partition_context.partition_id]['last_flush_datetime'] = on_event_batch_date_time
+    else:
+        print("min wait time not met")
+
+
+async def main(credential: DefaultAzureCredential, blob_storage_account_url: str, blob_container_name: str, event_hub_fully_qualified_namespace: str, event_hub_name: str):
+    # Create an Azure blob checkpoint store to store the checkpoints.
+    checkpoint_store = BlobCheckpointStore(
+        blob_account_url=blob_storage_account_url,
+        container_name=blob_container_name,
+        credential=credential,
+    )
+
+    # Create a consumer client for the event hub.
+    client = EventHubConsumerClient(
+        fully_qualified_namespace=event_hub_fully_qualified_namespace,
+        eventhub_name=event_hub_name,
+        consumer_group="test",
+        checkpoint_store=checkpoint_store,
+        credential=credential,
+    )
+
+    async with client:
+        await client.receive_batch(  # Replace with client.receive_batch()
+            on_event_batch=on_event_batch,
+            max_wait_time=1,
+            starting_position="-1",  # "-1" is from the beginning of the partition.
+            # prefetch=2,
+            max_batch_size=1000,
+        )
+
+    # Close credential when no longer needed.
+    await credential.close()
---
+++ src/run_script_rename.py
@@ -0,0 +1,19 @@
+import asyncio
+import nest_asyncio
+from src.eventhub_to_sa import main
+from azure.identity.aio import DefaultAzureCredential
+
+# TODO evne uitzoeken waar we deze moeten aanroepen. Hier, in eventhub_to_sa.py of beide
+nest_asyncio.apply()
+
+
+if __name__ == "__main__":
+    # TODO we kunnen deze misschien voor het gemak in een soort settings.py zetten
+    blob_storage_account_url = "https://checkpointweuovbiesondow.blob.core.windows.net"
+    blob_container_name = "event-hub-checkpoints"
+    # hardTODO Deze misschien handig in de keyvault dan kan je hem voor dev en prod zonder trucjes gebruiken
+    event_hub_fully_qualified_namespace = "dpmo-weu-eventhub-namespace-o-vbiesondow2u.servicebus.windows.net"
+    event_hub_name = 'vlog'
+    credential = DefaultAzureCredential()
+    # Run the main method.
+    asyncio.run(main(credential, blob_storage_account_url, blob_container_name, event_hub_fully_qualified_namespace, event_hub_name))

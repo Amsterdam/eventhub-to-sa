c1df624ca2fb638940061b73829088b5612ded27
Thomas Kok <thomas.kok@amsterdam.nl>
2024-04-10 13:51:27 +0200

    test only latest

--- src/eventhub_to_sa.py
+++ src/eventhub_to_sa.py
@@ -176,7 +176,7 @@ async def main(
             on_error=on_error,
             # TODO adjust max wait time
             max_wait_time=60,
-            starting_position="-1",  # "-1" is from the beginning of the partition.
+            starting_position="@latest",  # "-1" is from the beginning of the partition.
             # prefetch=2,
             max_batch_size=1000,
         )
